e0d14f2a5d4eb5f969ed5299a4d4065336e6618a
lucasfen <2440926317@qq.com>
2020-11-19 02:07:06 +0800

    mybatis config

--- mybatis-01/src/main/java/com/lucas/dao/UserMapper.java
+++ mybatis-01/src/main/java/com/lucas/dao/UserMapper.java
@@ -11,4 +11,12 @@ import java.util.List;
 public interface UserMapper {
 
     List<User> getUserList();
+
+    User getUserById(int id);
+
+    void addUser(User user);
+
+    void updateUser(User user);
+
+    void deleteUser(int id);
 }
--- mybatis-01/src/test/java/com/lucas/dao/UserMapperTest.java
+++ mybatis-01/src/test/java/com/lucas/dao/UserMapperTest.java
@@ -17,11 +17,21 @@ public class UserMapperTest {
     public void test() {
         SqlSession sqlSession = MybaitsUtil.getSqlSession();
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
         List<User> userList = mapper.getUserList();
         for (int i = 0; i < userList.size(); i++) {
             System.out.println(userList.get(i));
         }
 
+        User user = mapper.getUserById(1);
+
+        mapper.addUser(new User(2, "lucasfen", "123456"));
+
+        mapper.updateUser(new User(2, "fqh", "123"));
+
+        mapper.deleteUser(1);
+
+        sqlSession.commit();
         sqlSession.close();
     }
 }
